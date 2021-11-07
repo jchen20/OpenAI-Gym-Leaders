0136270409778fc9080e951c2d9be5893ee2998f
Chace Hayhurst <48729727+ChaceHayhurst@users.noreply.github.com>
2021-11-07 00:35:16 -0400

    models

---
+++ AgentTemplate.py
@@ -0,0 +1,21 @@
+from poke_env.player.player import Player
+
+def Model(Player):
+
+    def __init__(self):
+        super.__init__(self)
+
+    def training(self, env):
+        done = False
+        while not done:
+            act = self.choose_move(env.current_battle)
+            env.step(act)
+
+
+
+
+
+    def choose_move(self, battle):
+        moves = battle.available_moves
+        return moves[0]
+        
--- Main.py
+++ Main.py
@@ -1,16 +1,61 @@
-def train(env, model):
-    pass
+from poke_env.player.env_player import Gen8EnvSinglePlayer
+from AgentTemplate import Model
+
+class SimpleRLPlayer(Gen8EnvSinglePlayer):
+    def embed_battle(self, battle):
+        # -1 indicates that the move does not have a base power
+        # or is not available
+        moves_base_power = -np.ones(4)
+        moves_dmg_multiplier = np.ones(4)
+        for i, move in enumerate(battle.available_moves):
+            moves_base_power[i] = move.base_power / 100 # Simple rescaling to facilitate learning
+            if move.type:
+                moves_dmg_multiplier[i] = move.type.damage_multiplier(
+                    battle.opponent_active_pokemon.type_1,
+                    battle.opponent_active_pokemon.type_2,
+                )
+
+        # We count how many pokemons have not fainted in each team
+        remaining_mon_team = len([mon for mon in battle.team.values() if mon.fainted]) / 6
+        remaining_mon_opponent = (
+            len([mon for mon in battle.opponent_team.values() if mon.fainted]) / 6
+        )
+
+        # Final vector with 10 components
+        return np.concatenate(
+            [moves_base_power, moves_dmg_multiplier, [remaining_mon_team, remaining_mon_opponent]]
+        )
+
+    def compute_reward(self, battle) -> float:
+        return self.reward_computing_helper(
+            battle,
+            fainted_value=2,
+            hp_value=1,
+            victory_value=30,
+        )
+
 
 
 def main():
+    env_player = SimpleRLPlayer(battle_format="gen8randombattle")
 
-    env = gym.make()
-    state_size = env.observation_space.shape[0]
-    num_actions = env.action_space.n
+    # This call will finished eventual unfinshed battles before returning
 
-    model = 'a'
+    opponent = RandomPlayer(battle_format="gen8randombattle")
+    model = Model()
 
     
+    env_player.play_against(
+        env_algorithm=model.training,
+        opponent=opponent,
+        env_algorithm_kwargs={},
+    )
+
+
+
+
+
+
 
 
 
