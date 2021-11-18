f027b75d2945081a943c9b48ffd9747943c5f2b7
LostGeorge <george_hu@brown.edu>
2021-11-17 22:44:12 -0500

    Fix Q Learning step

--- dqn_agent.py
+++ dqn_agent.py
@@ -61,12 +61,13 @@ class DQNAgent(Player):
             with torch.set_grad_enabled(True):
                 state = torch.tensor([state]).float().to(self.device)
                 q_values = self.model(state)
-                target_q_values = q_values
-                # double check this
+                target_q_values = q_values.detach() # copies
+                # double check this // George: this should be correct now
                 if terminal:
-                    target_q_values[0][action] = rwd
+                    target_q_values[0, action] = rwd
                 else:
-                    target_q_values[0][action] = rwd + self.gamma * torch.max(torch.tensor([next_state]).float().to(self.device)).numpy()
+                    next_state = torch.tensor([next_state]).float().to(self.device)
+                    target_q_values[0, action] = rwd + self.gamma * torch.max(self.model(next_state)).detach()
                 loss = torch.sum((q_values - target_q_values) ** 2)
                 loss.backward()
                 self.optimizer.step()
