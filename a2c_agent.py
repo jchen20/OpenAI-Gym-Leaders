change entropy to kl divergence, remove dynamax, change embedding for a2cmove, fix a2cmove

a2c_agent.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
+from torch.distributions.kl import kl_divergence
 import numpy as np
 from poke_env.player.player import Player, BattleOrder
 from poke_env.player.battle_order import ForfeitBattleOrder
@@ -67,53 +68,61 @@ class A2CMove(nn.Module):
     def __init__(self, state_size, len_action_space):
         super().__init__()
         self.actor_move = nn.Sequential(
-            nn.Linear(112, 112),
+            nn.Linear(14, 56),
             nn.LeakyReLU(),
-            nn.Linear(112, 112),
+            nn.Linear(56, 56),
         )
         
-        self.actor = nn.Sequential(
-            nn.Linear(state_size, 1024),
+        self.actor_1 = nn.Sequential(
+            nn.Linear(state_size - 56, 1024 - 56*4),
             nn.LeakyReLU(),
+        )
+        self.actor_2 = nn.Sequential(
             nn.Linear(1024, 512),
             nn.LeakyReLU(),
             nn.Linear(512, len_action_space)
         )
         
         self.critic_move = nn.Sequential(
-            nn.Linear(112, 112),
+            nn.Linear(14, 56),
             nn.LeakyReLU(),
-            nn.Linear(112, 112),
+            nn.Linear(56, 56),
         )
         
-        self.critic = nn.Sequential(
-            nn.Linear(state_size, 1024),
+        self.critic_1 = nn.Sequential(
+            nn.Linear(state_size - 56, 1024 - 56*4),
             nn.LeakyReLU(),
+        )
+        self.critic_2 = nn.Sequential(
             nn.Linear(1024, 512),
             nn.LeakyReLU(),
             nn.Linear(512, 1)
         )
     
     def forward(self, x, mask):
-        policy_move = self.actor_move(x[:, :112])
-        policy = self.actor(torch.cat((policy_move, x[:, 112:]), dim=-1))
+        policy_move = self.actor_move(x[:, :56].reshape((-1, 4, 14))).flatten(start_dim=1)
+        policy_pre = self.actor_1(x[:, 56:])
+        policy = self.actor_2(torch.cat((policy_move, policy_pre), dim=-1))
         policy *= mask
         dist = Categorical(logits=policy)
 
-        values_move = self.critic_move(x[:, :112])
-        values = self.critic((torch.cat((values_move, x[:, 112:]), dim=-1)))
+        values_move = self.critic_move(x[:, :56].reshape((-1, 4, 14))).flatten(start_dim=1)
+        values_pre = self.critic_1(x[:, 56:])
+        values = self.critic((torch.cat((values_move, values_pre), dim=-1)))
         return dist, torch.squeeze(values)
     
     def actor_forward(self, x, mask):
-        policy_move = self.actor_move(x[:, :112])
-        policy = self.actor(torch.cat((policy_move, x[:, 112:]), dim=-1))
+        policy_move = self.actor_move(x[:, :56].reshape((-1, 4, 14))).flatten(start_dim=1)
+        policy_pre = self.actor_1(x[:, 56:])
+        policy = self.actor_2(torch.cat((policy_move, policy_pre), dim=-1))
         policy *= mask
         dist = Categorical(logits=policy)
         return dist
     
     def critic_forward(self, x):
-        values_move = self.critic_move(x[:, :112])
-        values = self.critic((torch.cat((values_move, x[:, 112:]), dim=-1)))
+        values_move = self.critic_move(x[:, :56].reshape((-1, 4, 14))).flatten(start_dim=1)
+        values_pre = self.critic_1(x[:, 56:])
+        values = self.critic_2((torch.cat((values_move, values_pre), dim=-1)))
         return torch.squeeze(values)
 
 
@@ -130,13 +139,13 @@ class A2CAgentFullTrajectoryUpdate(Player):
                 self.model = A2C(state_size + action_space, action_space)
         self.state_size = state_size
         self.action_space = action_space
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=2.5e-6)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=5e-6)
         self.scheduler = torch.optim.lr_scheduler.LambdaLR(self.optimizer, lambda _: 0.995)
         self.batch_size = batch_size # batch size is max horizon
         self.gamma = gamma
         self.gae_lambda = gae_lambda
         self.eps = 0.1
-        self.entropy_beta = 0.03 / np.log(action_space)
+        self.entropy_beta = 0.02 / np.log(action_space)
         self.alpha = 50
         self.embed_battle = None
         self.episode_reward = 0
@@ -147,6 +156,10 @@ class A2CAgentFullTrajectoryUpdate(Player):
         self.lambda_scale = self.lambda_scale.float()
         self.gamma_scale = self.gamma_scale.float()
         self.last_action = None
+
+        self.median_max_probs = []
+        self.steps = 0
+        self.cum_train_steps = []
 
         self.model.to(self.device)
     
@@ -171,12 +184,16 @@ class A2CAgentFullTrajectoryUpdate(Player):
             td_lambda_err = self.td_lambda_err(state, action, next_state, rwd, terminal)
             log_probs = dist.log_prob(action)
             actor_losses = -log_probs * td_lambda_err.detach()
-            ent_scale = self.entropy_beta * dist.entropy() * (1 + F.relu(-td_lambda_err.detach()))
-            actor_loss = (actor_losses - ent_scale).mean()
-            critic_loss = td_lambda_err.pow(2).mean()
+            mask_dist = Categorical((mask + 1e-10) / torch.sum(mask, dim=1, keepdim=True))
+            ent_scale = self.entropy_beta * kl_divergence(dist, mask_dist) * (1 + F.relu(-td_lambda_err.detach()))
+            actor_loss = (actor_losses + ent_scale).mean()
+            critic_loss = td_lambda_err.pow(2).mean()   
             loss = actor_loss + self.alpha * critic_loss
+
+            med_max_prob = torch.median(torch.max(dist.probs, dim=1)[0]).item()
+            self.median_max_probs.append(med_max_prob)
+            self.steps += 1
             if random.random() < 0.01:
-                med_max_prob = torch.median(torch.max(dist.probs, dim=1)[0]).item()
                 print(f'{actor_loss.item():.2E}', f'{critic_loss.item():.2E}', f'{med_max_prob:.2E}')
             
             loss.backward()
@@ -187,7 +204,7 @@ class A2CAgentFullTrajectoryUpdate(Player):
         done = False
         state, mask = env.reset()
         one_batch = deque([], maxlen=self.batch_size)
-        train_prob = 0.3
+        train_prob = 0.2
         self.last_action = None
         
         self.model.train()

main.py
@@ -33,27 +33,29 @@ def main():
     adversarial_train = False
 
     # Initialize agent
-    team_used = teams.wall_six_team
-    emb_dim = 371
+    team_used = teams.four_team
+    emb_dim = 315
+    move_encoder = False
 
     env_player = RLEnvPlayer(battle_format=bf, team=team_used)
     if method == 'dqn':
-        agent = DQNAgent(emb_dim, len(env_player.action_space) - 8,
+        agent = DQNAgent(emb_dim, len(env_player.action_space) - 12,
                          battle_format=bf, team=team_used)
     else:
         agent = A2CAgentFullTrajectoryUpdate(emb_dim,
-                                             len(env_player.action_space) - 8,
+                                             len(env_player.action_space) - 12,
+                                             move_encoder=move_encoder,
                                              battle_format=bf, team=team_used)
     agent.set_embed_battle(env_player.embed_battle)
 
     if adversarial_train:
         env_player2 = RLEnvPlayer(battle_format=bf, team=team_used)
         if method == 'dqn':
-            agent2 = DQNAgent(emb_dim, len(env_player.action_space) - 8,
+            agent2 = DQNAgent(emb_dim, len(env_player.action_space) - 12,
                               battle_format=bf, team=team_used)
         else:
             agent2 = A2CAgentFullTrajectoryUpdate(emb_dim, len(
-                env_player.action_space) - 8, battle_format=bf, team=team_used)
+                env_player.action_space) - 12, move_encoder=move_encoder, battle_format=bf, team=team_used)
         agent2.set_embed_battle(env_player2.embed_battle)
 
     # Initialize random player
@@ -145,6 +147,7 @@ def main():
                         env_algorithm=agent.train_one_episode,
                         opponent=agent2
                     )
+        agent.cum_train_steps.append(agent.steps)
 
         # Evaluate
         print('\nAgent 1:')
@@ -222,17 +225,25 @@ def main():
     print(agent_heur_wins)
 
     plt.figure()
-    plt.plot(episodes, agent_random_wins, '-b',
+    plt.plot(episodes, agent_random_wins, '-',
              label="Agent Wins against Random")
-    plt.plot(episodes, agent_max_dmg_wins, '-g',
+    plt.plot(episodes, agent_max_dmg_wins, '-',
              label="Agent Wins against Max Dmg")
-    plt.plot(episodes, agent_heur_wins, '-r',
+    plt.plot(episodes, agent_heur_wins, '-',
              label="Agent Wins against Heuristic")
     plt.xlabel("Episode")
     plt.ylabel("Number of Wins")
     plt.title("Agent Wins Per Episode")
     plt.legend()
-    plt.show()
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(agent.cum_train_steps, agent_heur_wins / n_eval_battles, '-r', label='Agent Winrate against Heuristic')
+    ax1.set_xlabel('steps')
+    ax1.set_ylabel('win rate')
+    ax2 = ax1.twinx()
+    ax2.plot(np.arange(agent.steps), agent.median_max_probs, '-k', linewidth=0.5, label='Median Max Probability Action Choice')
+    ax2.set_ylabel('max action probability')
+    fig.tight_layout()
 
     plt.figure()
     plt.semilogy(trainings, agent_max_dmg_rewards, '-g',

rl_env.py
@@ -43,29 +43,57 @@ class RLEnvPlayer(Gen8EnvSinglePlayer):
                     moves_dmg_multiplier[i] *= 1.5 # STAB
             moves_heal[i] = move.heal
         
-        dynamax_moves_base_power = -np.ones(4, dtype=int)
-        dynamax_moves_dmg_multiplier = np.zeros(4)
-        dynamax_moves_accuracy = np.zeros(4)
-        dynamax_moves_categories = np.zeros(4, dtype=int)
-        dynamax_moves_status = np.zeros(4, dtype=int)
-        dynamax_moves_heal = np.zeros(4, dtype=int)
-        for i, move in enumerate(battle.available_moves):
-            move = move.dynamaxed
-            dynamax_moves_base_power[i] = move.base_power / 100 # Simple rescaling to facilitate learning
-            dynamax_moves_accuracy[i] = move.accuracy
-            dynamax_moves_categories[i] = move.category.value
-            if move.status:
-                dynamax_moves_status[i] = move.status.value
-            else:
-                dynamax_moves_status[i] = 0
-            if move.type:
-                dynamax_moves_dmg_multiplier[i] = move.type.damage_multiplier(
-                    battle.opponent_active_pokemon.type_1,
-                    battle.opponent_active_pokemon.type_2,
-                )
-                if move.type in battle.active_pokemon.types:
-                    dynamax_moves_dmg_multiplier[i] *= 1.5 # STAB
-            dynamax_moves_heal[i] = move.heal
+        # dynamax_moves_base_power = -np.ones(4, dtype=int)
+        # dynamax_moves_dmg_multiplier = np.zeros(4)
+        # dynamax_moves_accuracy = np.zeros(4)
+        # dynamax_moves_categories = np.zeros(4, dtype=int)
+        # dynamax_moves_status = np.zeros(4, dtype=int)
+        # dynamax_moves_heal = np.zeros(4, dtype=int)
+        # for i, move in enumerate(battle.available_moves):
+        #     move = move.dynamaxed
+        #     dynamax_moves_base_power[i] = move.base_power / 100 # Simple rescaling to facilitate learning
+        #     dynamax_moves_accuracy[i] = move.accuracy
+        #     dynamax_moves_categories[i] = move.category.value
+        #     if move.status:
+        #         dynamax_moves_status[i] = move.status.value
+        #     else:
+        #         dynamax_moves_status[i] = 0
+        #     if move.type:
+        #         dynamax_moves_dmg_multiplier[i] = move.type.damage_multiplier(
+        #             battle.opponent_active_pokemon.type_1,
+        #             battle.opponent_active_pokemon.type_2,
+        #         )
+        #         if move.type in battle.active_pokemon.types:
+        #             dynamax_moves_dmg_multiplier[i] *= 1.5 # STAB
+        #     dynamax_moves_heal[i] = move.heal
+
+        moves_categories -= 1
+        category_matrix = np.zeros((4, 3))
+        category_matrix[np.arange(4), moves_categories] = 1
+        
+        status_matrix = np.zeros((4, 7))
+        for i, status_type in enumerate(moves_status):
+            if status_type != 0:
+                status_matrix[i, status_type - 1] = 1
+
+        moves_matrix = np.concatenate([
+            moves_base_power,
+            moves_dmg_multiplier,
+            moves_accuracy,
+            moves_heal,
+            category_matrix.flatten(),
+            status_matrix.flatten(),
+        ])
+        moves_matrix = moves_matrix.reshape((14, 4)).T
+
+        # dynamax_moves_categories -= 1
+        # dynamax_category_matrix = np.zeros((4, 3))
+        # dynamax_category_matrix[np.arange(4), dynamax_moves_categories] = 1
+
+        # dynamax_status_matrix = np.zeros((4, 7))
+        # for i, status_type in enumerate(dynamax_moves_status):
+        #     if status_type != 0:
+        #         dynamax_status_matrix[i, status_type - 1] = 1
 
         # We count how many pokemons have not fainted in each team
         remaining_mon_team = len([mon for mon in battle.team.values() if mon.fainted]) / 6
@@ -78,24 +106,6 @@ class RLEnvPlayer(Gen8EnvSinglePlayer):
     
         opponent_types = [t.value for t in battle.opponent_active_pokemon.types if t is not None]
         opponent_one_hot_types = one_hot(opponent_types, 18)
-        
-        moves_categories -= 1
-        category_matrix = np.zeros((4, 3))
-        category_matrix[np.arange(4), moves_categories] = 1
-        
-        status_matrix = np.zeros((4, 7))
-        for i, status_type in enumerate(moves_status):
-            if status_type != 0:
-                status_matrix[i, status_type - 1] = 1
-
-        dynamax_moves_categories -= 1
-        dynamax_category_matrix = np.zeros((4, 3))
-        dynamax_category_matrix[np.arange(4), dynamax_moves_categories] = 1
-
-        dynamax_status_matrix = np.zeros((4, 7))
-        for i, status_type in enumerate(dynamax_moves_status):
-            if status_type != 0:
-                dynamax_status_matrix[i, status_type - 1] = 1
         
         curr_stats = battle.active_pokemon.stats
         curr_stats_array = np.zeros(5)
@@ -176,18 +186,19 @@ class RLEnvPlayer(Gen8EnvSinglePlayer):
         
         # Final vector with many components
         state_vector = np.concatenate([
-            moves_base_power,
-            moves_dmg_multiplier,
-            moves_accuracy,
-            moves_heal,
-            category_matrix.flatten(),
-            status_matrix.flatten(),
-            dynamax_moves_base_power,
-            dynamax_moves_dmg_multiplier,
-            dynamax_moves_accuracy,
-            dynamax_moves_heal,
-            dynamax_category_matrix.flatten(),
-            dynamax_status_matrix.flatten(),
+            # moves_base_power,
+            # moves_dmg_multiplier,
+            # moves_accuracy,
+            # moves_heal,
+            # category_matrix.flatten(),
+            # status_matrix.flatten(),
+            moves_matrix.flatten(),
+            # dynamax_moves_base_power,
+            # dynamax_moves_dmg_multiplier,
+            # dynamax_moves_accuracy,
+            # dynamax_moves_heal,
+            # dynamax_category_matrix.flatten(),
+            # dynamax_status_matrix.flatten(),
             curr_one_hot_types,
             [curr_level, curr_hp],
             curr_stats_array,

utils.py
@@ -58,11 +58,11 @@ def player_action_to_move(player, action, battle):
         The battle will be forfeited.
     0 <= action < 4:
         The actionth available move in battle.available_moves is executed.
-    4 <= action < 8:
+    [DEPRECATED] 4 <= action < 8:
         The action - 4th available move in battle.available_moves is executed,
         while dynamaxing.
-    8 <= action < 14
-        The action - 8th available switch in battle.available_switches is executed.
+    4 <= action < 10
+        The action - 4th available switch in battle.available_switches is executed.
     If the proposed action is illegal, a random legal move is performed.
     :param action: The action to convert.
     :type action: int
@@ -79,15 +79,15 @@ def player_action_to_move(player, action, battle):
             and not battle.force_switch
     ):
         return player.create_order(battle.available_moves[action])
-    elif (
-            battle.can_dynamax
-            and 0 <= action - 4 < len(battle.available_moves)
-            and not battle.force_switch
-    ):
-        return player.create_order(battle.available_moves[action - 4],
-                                    dynamax=True)
-    elif 0 <= action - 8 < len(battle.available_switches):
-        return player.create_order(battle.available_switches[action - 8])
+    # elif (
+    #         battle.can_dynamax
+    #         and 0 <= action - 4 < len(battle.available_moves)
+    #         and not battle.force_switch
+    # ):
+    #     return player.create_order(battle.available_moves[action - 4],
+    #                                 dynamax=True)
+    elif 0 <= action - 4 < len(battle.available_switches):
+        return player.create_order(battle.available_switches[action - 4])
     else:
         return player.choose_random_move(battle)
 
@@ -98,12 +98,12 @@ def convert_real_action_to_pokeenv_action(action):
         return action - 8
 
 def get_valid_actions_mask(battle):
-    mask = np.zeros(14)
+    mask = np.zeros(10)
     if not battle.force_switch:
         ell = len(battle.available_moves)
         mask[0:ell] = 1
-        if battle.can_dynamax:
-            mask[4:4+ell] = 1
-    mask[8:8+len(battle.available_switches)] = 1
+        # if battle.can_dynamax:
+        #     mask[4:4+ell] = 1
+    mask[4:4+len(battle.available_switches)] = 1
 
     return mask